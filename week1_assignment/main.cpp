ca4e31d8edb7901345141dfa2213acebe3c3faf2
Robert Vari <mail.robertvari@gmail.com>
2026-05-23 10:29:51 +0200

    Init

---
+++ week1_assignment/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Geeksforgeeks's implementation with some changes
+
+struct Node
+{
+    int value{0};
+    Node* left;
+    Node* right;
+};
+
+
+int main(){
+    return 0;
+}
